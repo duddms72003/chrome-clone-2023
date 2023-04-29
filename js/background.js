d55311115a4b27faaa3996b3e346495f7db5a555
Jane <duddms72003@naver.com>
2023-04-29 18:37:15 +0900

    all

--- js/background.js
+++ js/background.js
@@ -1,13 +1,11 @@
 const images = [
   "0.jpeg",
-  "1.jpeg",
   "3.jpeg",
   "4.jpeg",
   "5.jpeg",
   "6.jpeg",
   "7.jpeg",
   "8.jpeg",
-  "9.jpeg",
 ];
 
 const chosenImage = images[Math.floor(Math.random() * images.length)];
